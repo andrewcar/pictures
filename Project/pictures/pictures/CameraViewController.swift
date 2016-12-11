9472c64d517751e810294938d0cd9264bdea8fca
Andrew Carvajal <andrewcarvajal1@gmail.com>
2016-12-11 06:08:42 -0500

    Removed boilerplate view controller code

--- Project/pictures/pictures/CameraViewController.swift
+++ Project/pictures/pictures/CameraViewController.swift
@@ -15,8 +15,6 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func cameraTapped(_ sender: UIButton) {
@@ -46,23 +44,6 @@ class CameraViewController: UIViewController {
         alertController.addAction(defaultAction)
         present(alertController, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // MARK: UINavigationControllerDelegate
--- Project/pictures/pictures/PageViewController.swift
+++ Project/pictures/pictures/PageViewController.swift
@@ -21,8 +21,6 @@ class PageViewController: UIPageViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         dataSource = self
         
@@ -30,22 +28,6 @@ class PageViewController: UIPageViewController {
             setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // MARK: UINavigationControllerDelegate
